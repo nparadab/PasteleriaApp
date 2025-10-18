3551df83dbb9d7a1135068b377dbb8f8e9e5e160
MrHuntter7 <an.pasarin@duocuc.cl>
2025-10-18 19:21:15 -0300

    prueba

--- app/src/main/java/com/proyecto1/milsabores/view/ListarProductosActivity.kt
+++ app/src/main/java/com/proyecto1/milsabores/view/ListarProductosActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 class ListaProductosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Cambio de prueba para commit
+        println("Commit de prueba funcionando")
         // No layout aún, tu compañera lo agregará
     }
 }
